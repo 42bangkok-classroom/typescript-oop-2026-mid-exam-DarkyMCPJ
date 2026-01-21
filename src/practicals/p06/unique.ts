ee7da7f7c67d391e280a32a8a8ff0bb0bc738647
unknown <68011141@kmitl.ac.th>
2026-01-21 16:05:00 +0700

    idk

--- src/practicals/p06/unique.ts
+++ src/practicals/p06/unique.ts
@@ -1,12 +1,17 @@
-function getUniqueNumbers(arr1: number[], arr2: number[]): number[] {
+export function getUniqueNumbers(array1: number[], array2: number[]): number[] {
   // Write your code below
-  const combinedArray = [...arr1, ...arr2];
-  const uniqueNumbers = Array.from(new Set(combinedArray));
-  return uniqueNumbers;
+  const unique1 = array1.filter(x => array2.indexOf(x) == -1);
 
+  const unique2 = array2.filter(x => array1.indexOf(x) == -1);
+
+  const merge = unique1.concat(unique2);
+
+  const duplicate = merge.filter((a, b) => merge.indexOf(a) !== b && merge.lastIndexOf(a) === b);
+
+  const noDuplicate = merge.filter(x => duplicate.indexOf(x) == -1);
+
+  return noDuplicate;
 }
 
 const arr1 = [1, 2, 3, 4];
 const arr2 = [3, 4, 5, 6];
-
-// Test
